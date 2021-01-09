cc0b6aa8cd8bac571a1015b16dd25bc8f6c91d4e
Jonatan Smith <jonatansmith@gmail.com>
2021-01-09 16:14:25 -0300

    How to use

--- firehose-new-line-transformation.py
+++ firehose-new-line-transformation.py
@@ -1,3 +1,10 @@
+#Python 3.8 Environment
+#This lambda code is executed on every record and add newline character (\n)
+#at the end of each JSON object found on each record
+#Search tags: Firehose newline char, Firehose multiple inline JSON Objects, Firehose JSON separator
+#How to use:
+##Create a Lambda python 3.8 
+##Configure Firehose transformation and use created lambda
 import base64
 import json
 import re
